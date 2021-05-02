c3866fac459cd1a4ddabfb2ce869d1da007446cc
leeyuchang <leeyucha@gmail.com>
2021-05-02 11:30:46 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>header</div>
       </header>
+      <content className="App-content">
+        <div className="App-content-item">1</div>
+        <div className="App-content-item">1</div>
+        <div className="App-content-item">1</div>
+        <div className="App-content-item">1</div>
+        <div className="App-content-item">1</div>
+        <div className="App-content-item">1</div>
+        <div className="App-content-item">1</div>
+        <div className="App-content-item">1</div>
+        <div className="App-content-item">1</div>
+        <div className="App-content-item">1</div>
+        <div className="App-content-item">1</div>
+        <div className="App-content-item">1</div>
+        <div className="App-content-item">1</div>
+        <div className="App-content-item">1</div>
+        <div className="App-content-item">1</div>
+        <div className="App-content-item">1</div>
+      </content>
+      <footer className="App-footer">
+        <div>footer</div>
+      </footer>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
